767f6d31bcbd581fb97c598b669f0a492b0a6bcd
Tavi Greenfield <tavi@elyandpilar.com>
2021-09-16 10:57:18 -0700

    wrote char methods code

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,28 +4,49 @@ public class MyMain {
     // If the char is not an uppercase letter, 
     // it is returned unchanged. 
     public static char toLower(char ch) {
-        // REPLACE WITH YOUR CODE
-        return '!';
+        if (!(ch>='A'&& ch<='Z')){
+            return ch;
+        }
+        int x=ch+32;
+        return (char)x;
     }
 
     // Changes a lowercase letter to an uppercase letter
     // If the char is not a lowercase letter,
     // it is returned unchanged.
     public static char toUpper(char ch) {
-        // REPLACE WITH YOUR CODE HERE
-        return '!';
+        if (!(ch>='a'&& ch<='z')) {
+            return ch;
+        }
+        int x=ch-32;
+        return (char)x;
     }
 
     // The method is given a String as input and returns a
     // new String where all of the numbers have been removed
     // from the original String.
     public static String removeNumbers(String str) {
-        // REPLACE WITH YOUR CODE
-        return "!!!";
+        String x = "";
+        for (int i=0; i<(str.length());i++){
+            char ch = str.charAt(i);
+            if (!(ch>=48 && ch<=57)){
+                x= x + ch;
+            }
+        }
+        return x;
     }
 
     // Write some code to test your methods!
     public static void main(String[] args) {
-        // ADD SOME TESTING CODE HERE
+        System.out.println("to lower");
+        System.out.println(toLower('A'));//a
+        System.out.println(toLower('c'));//c
+        System.out.println(toLower('J'));//j
+        System.out.println("to upper");
+        System.out.println(toUpper('d'));//D
+        System.out.println(toUpper('K'));//K
+        System.out.println("remove number tests");
+        System.out.println(removeNumbers("54873h324i"));//hi
+        System.out.println(removeNumbers("35n3a54th23an"));//nathan
     }
 }
